ef88515dcdbc710989b401003990eee41ce1b9f2
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-01-05 11:57:31 -0500

    Update StudInfo.java

--- StudInfo.java
+++ StudInfo.java
@@ -4,3 +4,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+
+enum Gradelevel { 
+        
+        twelve("12"),
+        eleven("11"),
+        ten("10"),
+        nine("9"),
+        eight("8"),
+        seven("7"),
+        six("6"),
+        five("5"),
