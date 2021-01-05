3188cad8bf419ab664fa4f86a87fae2071d21a28
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-01-05 11:56:55 -0500

    Create StudInfo.java

---
+++ StudInfo.java
@@ -0,0 +1,6 @@
+//Scan all primitive data types
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
